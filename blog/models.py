0bc48a5b979f4f2d8897a0a30bcb69e8a7edd7cd
Spook <petar.peki1@gmail.com>
2022-04-29 11:57:11 +0200

    Added post and image models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    created_by = models.ForeignKey(User,
+        on_delete=models.CASCADE,
+        related_name="posts")
+    text = models.TextField()
+    
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Image(models.Model):
+    post = models.ForeignKey(Post,
+        on_delete=models.CASCADE,
+        related_name="images")
+    image = models.ImageField()
